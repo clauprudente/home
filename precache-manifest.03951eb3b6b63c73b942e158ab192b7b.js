6fa0516975de643ab617ff7e81874dfee317f3cd
Claudia Prudente <claudiamary.santos@gmail.com>
2021-06-03 16:44:08 -0300

    Updates

---
+++ precache-manifest.03951eb3b6b63c73b942e158ab192b7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfdb2397321b1f4d4cfeac4ecb35a77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49993fea96c647eaa208",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d3c6d503091e9d1dc76c",
+    "url": "/static/css/main.46eb6c73.chunk.css"
+  },
+  {
+    "revision": "49993fea96c647eaa208",
+    "url": "/static/js/2.3a26df39.chunk.js"
+  },
+  {
+    "revision": "44b6ea107d5f6954b31204b3600b6c0e",
+    "url": "/static/js/2.3a26df39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c6d503091e9d1dc76c",
+    "url": "/static/js/main.af3426c6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "1210ac5fb8c910743c07a26c5a119779",
+    "url": "/static/media/Profile.1210ac5f.jpg"
+  }
+]);
